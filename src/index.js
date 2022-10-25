f79d503a02648ef34c8ee0765803589bf7d066c8
Anton <kr1sone@mail.ru>
2022-10-25 21:42:33 +0300

    yarn

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import "./index.css"
 
 ReactDOM.render(
-	<App/>,
+	<div className="wrapper">
+		<div className="card">=)</div>
+		<div className="card">O_o</div>
+		<div className="card">^_^</div>
+		<div className="card">=(</div>
+	</div>,
 	document.querySelector("#root")
-);
+)
